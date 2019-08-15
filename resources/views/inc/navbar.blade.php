b430955e8eede5791ca2b900774ec3df1e9ac291
Etienne Laramee <elaramee@biointelligence.com>
2019-08-14 20:01:41 -0400

    Add support for blog page and post pages, + pagination

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -14,6 +14,9 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/posts">Blog</a>
           </li>
         </ul>
       </div>
